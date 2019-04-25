8f9764ddb0883f8140dc14b302094db0eefc0c82
季刚 <jigang.yf@alibaba-inc.com>
2019-04-25 17:20:01 +0800

    fixbug

--- src/main/scala/com/ifan/Java.java
+++ src/main/scala/com/ifan/Java.java
@@ -1,5 +1,10 @@
 package com.ifan;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Java {
     public static void main(String[] args) {
         int i = Scala.add(1,2);
@@ -37,6 +42,41 @@ public class Java {
         {
             s.charAt(m);
         }
+
+        String str = "2314(*)";
+
+        String[] str1 = str.split("\\(");
+        for (String ss : str1)
+        {
+            System.out.println(ss);
+        }
+
+        System.out.println("*");
+
+        Pattern PATTERN = Pattern.compile("ds\\s*=\\s*'*\\d+'*");
+        String str11 = "INSERT OVERWRITE TABLE aliwx_ods.s_attr_thirdtouserid PARTITION (ds = '20141209') SELECT ds FROM aliwx_ods.s_attr_thirdtouserid WHERE ds = '20141209'";
+        Matcher matcher = PATTERN.matcher(str11.toLowerCase());
+
+
+        List<String> list = new ArrayList<String>();
+        while (matcher.find()) {
+            list.add(matcher.group());
+        }
+
+        for (int j=0; j<list.size(); j++)
+        {
+            System.out.println(list.get(j));
+        }
+
+        System.out.println(" \" \' \n \t \\");
+        Pattern pattern = Pattern.compile("\'+\"+");
+        Matcher matcher1 = pattern.matcher("\n\t");
+
+        System.out.println(matcher1.find());
+        System.out.println(".");
+
+
+
 
     }
 }
